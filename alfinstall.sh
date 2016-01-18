5e25506d791f7dc7400274d301fceefb04bb5b4d
Peter Löfgren <peter@loftux.se>
2016-01-18 10:25:57 +0100

    Remvind install of swftools as Alfresco has removed support for swf transformations.

--- alfinstall.sh
+++ alfinstall.sh
@@ -399,35 +399,6 @@ else
 fi
 
 echo
-echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-echo "Install Swftools."
-echo "This will download and install swftools used for transformations to Flash."
-echo "Since the swftools Ubuntu package is not included in all versions of Ubuntu,"
-echo "this install downloads from swftools.org and compiles."
-echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-read -e -p "Install Swftools${ques} [y/n] " -i "n" installswftools
-
-if [ "$installswftools" = "y" ]; then
-  echoblue "Installing build tools and libraries needed to compile swftools. Fetching packages..."
-  sudo apt-get $APTVERBOSITY install make build-essential ccache g++ libgif-dev libjpeg62-dev libfreetype6-dev libpng12-dev libt1-dev
-  cd /tmp/alfrescoinstall
-  echo "Downloading swftools..."
-  curl -# -O $SWFTOOLS
-  tar xf swftools*.tar.gz
-  cd "$(find . -type d -name "swftools*")"
-  ./configure
-  sudo make && sudo make install
-  echo
-  echogreen "Finished installing Swftools"
-  echo
-else
-  echo
-  echo "Skipping install of Swftools."
-  echored "Remember to install swftools via Ubuntu packages or by any other mean."
-  echo
-fi
-
-echo
 echoblue "Adding basic support files. Always installed if not present."
 echo
 # Always add the addons dir and scripts
