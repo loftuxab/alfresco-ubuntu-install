21b14581e0b96c5bd31f8444bff2bed2f8d95ae1
Peter Löfgren <peter@loftux.se>
2017-01-20 09:34:18 +0100

    Update Mariadb script to use valid keys for apt repo

--- scripts/mariadb.sh
+++ scripts/mariadb.sh
@@ -21,9 +21,9 @@ echo
 
 read -e -p "Install MariaDB? [y/n] " -i "n" installmariadb
 if [ "$installmariadb" = "y" ]; then
-  sudo apt-get install python-software-properties
-  sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
-  sudo add-apt-repository "deb http://mirror2.hs-esslingen.de/mariadb/repo/10.0/ubuntu $(lsb_release -cs) main"
+  sudo apt-get install software-properties-common
+  sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+  sudo add-apt-repository "deb [arch=amd64,i386,ppc64el] http://ftp.ddg.lth.se/mariadb/repo/10.1/ubuntu $(lsb_release -cs) main"
   sudo apt-get update
   sudo apt-get install mariadb-server
 fi
