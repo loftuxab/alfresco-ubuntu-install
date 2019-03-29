c5e5064e44c50b8ded3d7fd28afa9f1cb73f8006
Peter Löfgren <peter@loftux.se>
2019-03-29 14:53:01 +0100

    Switch to openJDK. Issue #105

--- alfinstall.sh
+++ alfinstall.sh
@@ -408,23 +408,28 @@ fi
 echo
 echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 echo "Install Java JDK."
-echo "This will install Oracle Java 8 version of Java. If you prefer OpenJDK"
+echo "This will install OpenJDK 8 version of Java. If you prefer Oracle Java 8 "
 echo "you need to download and install that manually."
 echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 read -e -p "Install Oracle Java 8${ques} [y/n] " -i "$DEFAULTYESNO" installjdk
 if [ "$installjdk" = "y" ]; then
-  echoblue "Installing Oracle Java 8. Fetching packages..."
-  sudo apt-get $APTVERBOSITY install python-software-properties software-properties-common
-  sudo add-apt-repository ppa:webupd8team/java
-  sudo apt-get $APTVERBOSITY update
-  echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-  sudo apt-get $APTVERBOSITY install oracle-java8-installer
-  sudo update-java-alternatives -s java-8-oracle
+  echoblue "Installing OpenJDK..."
+  sudo apt-get $APTVERBOSITY install openjdk-8-jre-headless
   echo
-  echogreen "Finished installing Oracle Java 8"
+  echogreen "Make sure correct default java is selected!"
+  echo
+  sudo update-alternatives --config java
+  #sudo apt-get $APTVERBOSITY install python-software-properties software-properties-common
+  #sudo add-apt-repository ppa:webupd8team/java
+  #sudo apt-get $APTVERBOSITY update
+  #echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+  #sudo apt-get $APTVERBOSITY install oracle-java8-installer
+  #sudo update-java-alternatives -s java-8-oracle
+  echo
+  echogreen "Finished installing OpenJDK "
   echo
 else
-  echo "Skipping install of Oracle Java 8"
+  echo "Skipping install of OpenJDK."
   echored "IMPORTANT: You need to install other JDK and adjust paths for the install to be complete"
   echo
 fi
